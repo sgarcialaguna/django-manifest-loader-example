be9c927e7f9f2f0f797dab020e4901591e7c88b5
Santiago Garcia <santiago.garcia@posteo.de>
2021-01-30 14:28:33 +0100

    Set up react, babel and all that jazz

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert('Hello World')
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
+import React from 'react'
+import { render } from 'react-dom'
+
+render(<div>Hello from React</div>, document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/index.js',
   plugins: [
       new CleanWebpackPlugin(),  
@@ -13,4 +15,19 @@ module.exports = {
     filename: '[name].[contenthash].js', 
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react']
+            }
+          }
+        }
+      ]
+  }
+  
 };
